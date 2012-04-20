5a4ec24638efa094fc67888071c4f88853645caf
Daniel Miller <bonsaiviking@gmail.com>
2012-04-19 21:52:27 -0500

    New attack, documentation, refactor

--- tarpyt.py
+++ tarpyt.py
@@ -17,39 +17,58 @@ class Tarpyt(object):
         for verb in ('GET','POST','HEAD'):
             setattr(self.handler_class, 'do_'+verb, lambda h: self.pick_response(h)(h))
         self.handler_class.server_version = 'Apache/1.3.14'
-        self.page_string = "<html><head><title>Welcome to the Labyrinth</title></head><body><ul>{0}</ul></body></html>"
-        self.link_string = '<li><a href="{0}">{0}</a></li>'
         self.builder = builder
+        if self.builder:
+            def getlink(path='/'):
+                pathlist = ['^']
+                pathlist.extend(path.split('/'))
+                elem = self.builder.uripaths.get(pathlist[-1])
+                if elem == '$':
+                    elem = chr(random.randint(0x61,0x7A))
+                return '/'.join(pathlist[1:]+[elem])
+            self.getlink = getlink
+        else:
+            def getlink(path='/'):
+                next_path = path.rstrip('/') + '/{0}'
+                return next_path.format(chr(random.randint(0x61,0x7A)))
+            self.getlink = getlink
         self.responses = []
-        self.responses.extend( (self.response_linkpage,) * 3 )
+        self.responses.extend( (self.response_linkpage,) * 4 )
         self.responses.extend( (self.response_redirect,) * 1 )
         self.responses.extend( (self.response_inf_redirect,) * 1 )
+        self.responses.extend( (self.response_oversize,) * 1 )
 
     def response_linkpage(self, handler):
+        """ Category: tarpit
+        Returns an html page full of links
+        """
+        page_string = "<html><head><title>Welcome to the Labyrinth</title></head><body><ul>{0}</ul></body></html>"
+        link_string = '<li><a href="{0}">{0}</a></li>'
         links = []
-        getlink = None
-        if self.builder:
-            pathlist = ['^']
-            pathlist.extend(handler.path.split('/')[:-1])
-            getlink = lambda: '/'.join(pathlist[1:]+[self.builder.uripaths.get(pathlist[-1])]).rstrip('$')
-        else:
-            next_path = handler.path.rstrip('/') + '/{0}'
-            getlink = lambda: next_path.format(chr(random.randint(0x61,0x7A)))
         for n in range(0,5):
-            href = getlink()
-            links.append(self.link_string.format(href))
-        response_body = self.page_string.format(''.join(links))
+            href = self.getlink(handler.path)
+            links.append(link_string.format(href))
+        response_body = page_string.format(''.join(links))
         handler.send_response(200)
         handler.send_header('Content-Length',len(response_body))
         handler.end_headers()
         handler.wfile.write(response_body)
 
     def response_redirect(self, handler):
+        """ Category: realism
+        Redirects to a random page
+        """
         handler.send_response(302)
-        handler.send_header('Location','/'+chr(random.randint(0x61,0x7A)))
+        handler.send_header('Location',self.getlink())
         handler.end_headers()
 
     def response_inf_redirect(self, handler):
+        """ Category: tarpit
+        Returns a 302 redirect to a page which has the same modulus as the
+        one requested, resulting in an infinite redirect. Loops eventually.
+        If a suitable redirect cannot be made, falls back to appending a
+        random path element to the path requested.
+        """
         modulus = len(self.responses)
         newpath = handler.path
         tmp = 0
@@ -72,11 +91,29 @@ class Tarpyt(object):
                 break
         if pos != 0:
             newpath = newpath[:pos] + chr(tmp) + newpath[pos+1:]
+        else:
+            newpath = self.getlink(newpath)
         handler.send_response(302)
         handler.send_header('Location',newpath)
         handler.end_headers()
 
+    def response_oversize(self, handler):
+        """ Category: attack
+        Sends an oversized Content-Length header. Some web servers have had
+        Denial of Service vulnerabilities due to preallocating memory or disk
+        (e.g. https://secunia.com/advisories/35645). Some spiders may have
+        similar vulns (See, e.g., feature request for wget: 
+        https://lists.gnu.org/archive/html/bug-wget/2012-01/msg00054.html)
+        """
+        handler.send_response(200)
+        handler.send_header('Content-Length',4 * 2**30) #4GB
+        handler.end_headers()
+
     def response_robots(self, handler):
+        """ Category: safety
+        Sends a robots.txt which disallows all paths for all robots. This
+        should guarantee no innocent spiders get caught in our tarpit.
+        """
         robots = "User-agent: *\nDisallow: /\n"
         handler.send_response(200)
         handler.send_header('Content-Length',len(robots))
